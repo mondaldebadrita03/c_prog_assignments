135129c461dab19333c96b691ae6899e40154279
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2023-04-21 10:58:37 +0530

    Update printpattern1.c

--- printpattern1.c
+++ printpattern1.c
@@ -12,10 +12,10 @@
 */
 #include<stdio.h>
 int main(void) {
-    int n=10,i,j;               // variable declaration
+    int n=10,i,j;                  // variable declaration
     for(i=1; i<=n; i++) {          //logic for the structure
         for(j=1; j<=i; j++) {      //logic for the structure
-            printf("%d ",n-j);      //logic for the pattern
+            printf("%d ",n-j);     //logic for the pattern
         }
         printf("\n");
     }
